267be8ae0e405f86393a849a0dcb7ea6dcbf26d9
Zverinus <75039044+Zverinus@users.noreply.github.com>
2021-03-23 11:10:23 +0300

    ex4

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ def promotion_image():
                     href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
                     integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
                     crossorigin="anonymous">
+                    <title>Колонизация</title>
                   </head>
                   <body>
                     <h1>Жди нас, Марс!</h1>
